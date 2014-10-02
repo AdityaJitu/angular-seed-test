11d2e0ddcdda9ed50c6914b9fd1dad8ee98cb1c7
AdityaJitu <adityapadhi2010@yahoo.in>
2014-10-02 10:17:16 +0530

    Update controller.js
    Write a directive

--- js/controller.js
+++ js/controller.js
@@ -4,4 +4,5 @@
 angular.module('myApp.controller',[]).
 controller('homeCtrl',['$log',function($log){
         $log.info("you are inside homeCtrl");
+        $log.info("Hey!! you try to ngModelController!!");
     }]);
